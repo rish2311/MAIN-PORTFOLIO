0345e67759db5aa6576d806441ef33bbc08e5b9f
mrayushmehrotra <ayushmehrotra099@gmail.com>
2024-05-19 19:05:16 +0530

    fixed UI

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
@@ -10,27 +11,48 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/spotify.webp"
-          title="Spotify Next.js Clone"
-          description="A Spotify Clone Build in Nextjs with Supabase sql database , radix UI, zustand, while following clean code while following OOPS, "
-        />
-        <ProjectCard
-          src="/notion.jpg"
-          title="Notion AI Clone"
-          description="A Notion Clone  build in Next js with OPENAI API Integration with using tanstack, and Clerk Auth and Using Quill library."
-        />
-        <ProjectCard
-          src="/ecommerce.webp"
-          title="Ecommerce Backend"
-          description="A Ecommerce Backend built in Nodejs and Express, with Middleware , Authentication , Admin section , Functionality a Ecommerce have ."
-        />
-        <ProjectCard
-          src="/zip.png"
-          title="Zipper"
-          description="A Complete File Zipper and UnZipper built in Rust and GO follows Data compression pattern "
-        />
+      <div className="h-full z-30   w-full flex flex-col md:flex-row gap-10 px-10">
+        <a
+          href="https://github.com/imrishabhmehrotra/Face-Recognition-Attendence-Project"
+          target="_blank"
+        >
+          <ProjectCard
+            src="/face-project1.webp"
+            title="Medicine Recommender System"
+            description="
+          Spearheaded the development and implementation of an advanced attendance system leveraging state-of-the-art image
+          detection and face recognition algorithms.
+          Successfully increased efficiency and accuracy in attendance monitoring processes by eliminating manual data entry
+          errors and streamlining attendance management workflows.
+          
+          "
+          />
+        </a>
+        <a
+          href="https://github.com/imrishabhmehrotra/Medicine-Recommender-System"
+          target="_blank"
+        >
+          <ProjectCard
+            src="/medicine-project2.png"
+            title="Medicine Recommender System"
+            description="
+          Developed a cutting-edge Medicine Recommendation System that integrated sentiment analysis of user reviews, leading
+          to a 30% boost in medication effectiveness and a 20% decrease in treatment costs.
+          Designed the system with scalability in mind. And Optimized system performance for quick response times, enhancing
+          the overall user experience."
+          />
+        </a>
+        <a href="https://github.com/imrishabhmehrotra/CHATSHAT" target="_blank">
+          <ProjectCard
+            src="/chat-shat-project3.jpeg"
+            title="ChatShat"
+            description="
+            Chatting app allows you to communicate with your customers in web chat rooms.
+          Full stack Chat Application for real time interaction between users.
+          Led the development of a real-time full-stack chat application using React.js, Redux Toolkit, Node.js, and Express.js.
+          The app enables fast and seamless real-time interaction between users."
+          />
+        </a>
       </div>
     </div>
   );
